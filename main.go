b1c25db1fdb27403466d4dd403f386fd63f83857
彭勇 <pengyong@iluckycloud.com>
2024-09-05 09:41:42 +0800

    update dawn

--- main.go
+++ main.go
@@ -119,8 +119,8 @@ func convertToGrayScale(imgBase64 string) string {
 
 func ping(email string) {
 	rand.Seed(time.Now().UnixNano())
-	client := resty.New().SetProxy("http://2892ED58F5DF1579-residential-country_US-r_0m-s_PDfBsmnJTM:Qbb645Mf@gw-us.nstproxy.com:24125").
-		//client := resty.New().
+	//client := resty.New().SetProxy("http://2892ED58F5DF1579-residential-country_US-r_0m-s_PDfBsmnJTM:Qbb645Mf@gw-us.nstproxy.com:24125").
+	client := resty.New().
 		SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true}).
 		SetHeader("content-type", "application/json").
 		SetHeader("origin", "chrome-extension://fpdkjdnhkakefebpekbdhillbhonfjjp").
